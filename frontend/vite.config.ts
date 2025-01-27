e9643ded12e905430bc4ea93c8fec88d86e74ec5
elect10 <wlgnsakswp@naver.com>
2025-01-28 03:46:01 +0900

    test: sub domain deploy test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        student: "./student/index.html",
+        admin: "./admin/index.html",
+      },
+    },
+    outDir: "dist",
+  },
 });
