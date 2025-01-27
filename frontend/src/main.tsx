158b5cefea0e9cf6ee0d65ac5b552e9637bc5532
elect10 <wlgnsakswp@naver.com>
2025-01-28 04:55:43 +0900

    test

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import StudentApp from './domains/student/App';
-import AdminApp from './domains/admin/App';
+// import './index.css';
+import StudentApp from './domains/student/App.tsx';
+import AdminApp from './domains/admin/App.tsx';
 
-const hostname = window.location.hostname;
-const isAdmin = hostname.startsWith('admin');
+const isAdmin = window.location.pathname.startsWith('/admin');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>{isAdmin ? <AdminApp /> : <StudentApp />}</StrictMode>,
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,13 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-        student: "./student/index.html",
-        admin: "./admin/index.html",
-      },
-    },
     outDir: "dist",
   },
 });
