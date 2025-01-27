3df14af8b976f26b830c2198e18f3a131b8c7d0b
elect10 <wlgnsakswp@naver.com>
2025-01-28 04:32:46 +0900

    fix: typo

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import StudentApp from './domains/student/App.tsx';
-import AdminApp from './domains/admin/App.tsx';
+import AdminApp from './domains/admin/AdminApp.tsx';
 
 const hostname = window.location.hostname;
 const isAdmin = hostname.startsWith('admin');
