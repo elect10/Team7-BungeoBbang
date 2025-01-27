7861f66c60763641f138b9f7bc46a2ed638ddfd9
elect10 <wlgnsakswp@naver.com>
2025-01-28 04:39:38 +0900

    fix: typo

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,8 +4,7 @@ import { createRoot } from 'react-dom/client';
 import StudentApp from './domains/student/App.tsx';
 import AdminApp from './domains/admin/App.tsx';
 
-const hostname = window.location.hostname;
-const isAdmin = hostname.startsWith('admin');
+const isAdmin = window.location.pathname.startsWith('/admin');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>{isAdmin ? <AdminApp /> : <StudentApp />}</StrictMode>,
